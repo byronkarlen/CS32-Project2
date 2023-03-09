7110155dc320e5178fba57bb8039e2cca8109cde
Byron Karlen <72267902+byronkarlen@users.noreply.github.com>
2023-03-09 13:03:10 -0800

    Update main.cpp to get rid of arbitrary tests

--- main.cpp
+++ main.cpp
@@ -12,13 +12,5 @@
 using namespace std;
 
 int main(){
-    WeddingGuest w;
-    w.inviteGuest("Byron", "Karlen", "abc");
-    w.inviteGuest("byron", "Karlen", "abc");
-    w.inviteGuest("Byron", "Larlen", "abc");
-    WeddingGuest w2;
-    w2.inviteGuest("Byron", "Larlen", "Abc");
-    WeddingGuest output;
-    cout << joinGuests(w, w2, output);
-    cout << output.guestCount();
+    //Add tests here
 }
